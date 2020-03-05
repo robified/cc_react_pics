0961ea32be43ad558b9556a06180c8fed42bb10f
Robin <daorob@gmail.com>
2020-03-05 04:45:22 -0600

    Handle query request using async await successful.

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,18 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: 'Client-ID YOUR_ACCESS_KEY'
+    async onSearchSubmit(term) {
+        const response = await axios.get(
+            'https://api.unsplash.com/search/photos',
+            {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID YOUR_ACCESS_KEY'
+                }
             }
-        });
+        );
+
+        console.log(response.data.results);
     }
 
     render() {
