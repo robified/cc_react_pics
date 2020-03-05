78b0160868ba38b324b5ec1a0752a84ec9d81e96
Robin <daorob@gmail.com>
2020-03-05 05:13:17 -0600

    Refactor Unsplash api request into its on separate api component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     state = { images: [] };
 
     onSearchSubmit = async term => {
-        const response = await axios.get(
+        const response = await unsplash.get(
             'https://api.unsplash.com/search/photos',
             {
-                params: { query: term },
-                headers: {
-                    Authorization: 'Client-ID YOUR_ACCESS_KEY'
-                }
+                params: { query: term }
             }
         );
 
